538aea63e24b92e726139316686906ee8aa02e61
Robert Lester <rlester@roberts-macbook-pro.local>
2021-05-04 21:18:06 +0000

    ..

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -66,7 +66,9 @@ function showAsyncChannels(formVals) {
 	console.log(formVals);
 	
 	$('.paragraph-19-copy').append($('.thumbs-up'));
-	var dismiss = $('<div>').addClass('dismiss-line');
+	var dismiss = $('<div>').addClass('dismiss-line').mousedown(function() {
+		$('.flipped').addClass('dismissed');
+	});
 	$('.async-channels').after(dismiss);
 	
 	var abcVisible = navigator.userAgent.match(/iPhone|iPad|iPod/i) || navigator.userAgent.match(/Mac OS X/i);
@@ -94,52 +96,3 @@ function showAsyncChannels(formVals) {
 	$('.async-channels').append(abcLink).append(fbmLink).append(waLink);
 }
 
-document.addEventListener('touchstart', handleTouchStart, false);        
-document.addEventListener('touchmove', handleTouchMove, false);
-
-var xDown = null;                                                        
-var yDown = null;
-
-function getTouches(evt) {
-  return evt.touches ||             // browser API
-		 evt.originalEvent.touches; // jQuery
-}                                                     
-
-function handleTouchStart(evt) {
-	const firstTouch = getTouches(evt)[0];                                      
-	xDown = firstTouch.clientX;                                      
-	yDown = firstTouch.clientY;                                      
-};                                                
-
-function handleTouchMove(evt) {
-	if (evt.target.className.indexOf('flipped') < 0) {
-		return;
-	}
-	
-	if ( ! xDown || ! yDown ) {
-		return;
-	}
-
-	var xUp = evt.touches[0].clientX;                                    
-	var yUp = evt.touches[0].clientY;
-
-	var xDiff = xDown - xUp;
-	var yDiff = yDown - yUp;
-
-	if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-		if ( xDiff > 0 ) {
-			/* left swipe */ 
-		} else {
-			/* right swipe */
-		}                       
-	} else {
-		if ( yDiff > 0 ) {
-			$('.flipped.').addClass('dismissed');
-		} else { 
-			/* down swipe */
-		}                                                                 
-	}
-	/* reset values */
-	xDown = null;
-	yDown = null;                                             
-};
