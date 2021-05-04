7e176597cc743bc7b213d7d037ad40bf601ce318
Robert Lester <rlester@roberts-macbook-pro.local>
2021-05-04 21:11:26 +0000

    ..

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -95,3 +95,50 @@ function showAsyncChannels(formVals) {
 	
 	$('.async-channels').append(abcLink).append(fbmLink).append(waLink);
 }
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+		 evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+	const firstTouch = getTouches(evt)[0];                                      
+	xDown = firstTouch.clientX;                                      
+	yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+	console.log(evt);
+	if ( ! xDown || ! yDown ) {
+		return;
+	}
+
+	var xUp = evt.touches[0].clientX;                                    
+	var yUp = evt.touches[0].clientY;
+
+	var xDiff = xDown - xUp;
+	var yDiff = yDown - yUp;
+
+	if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+		if ( xDiff > 0 ) {
+			/* left swipe */ 
+		} else {
+			/* right swipe */
+		}                       
+	} else {
+		if ( yDiff > 0 ) {
+			/* up swipe */ 
+		} else { 
+			/* down swipe */
+		}                                                                 
+	}
+	/* reset values */
+	xDown = null;
+	yDown = null;                                             
+};
