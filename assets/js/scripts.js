06bda84e3a05d796490c02f17ccd5e39653003d7
Robert Lester <rlester@roberts-macbook-pro.local>
2021-05-07 17:48:31 +0000

    ..

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -134,7 +134,9 @@ function appendDismissLine() {
 			//if (distance * -1 > 0) return;
 			var newHeight = touchHeight - (distance * .6);
 			$('.flipped').height(touchHeight - (distance * .6));
-			$(document).scrollTop(currentScrollTop - (260 - newHeight));
+			
+			console.log(currentScrollTop - (260 - newHeight));
+			//$(document).scrollTop(currentScrollTop - (260 - newHeight));
 			//$('.flipped .card-back, .flipped .card-back-smb').css('bottom', -1 * (distance * .6) );
 			//$('.flipped').css('top', distance * -1);
 			
