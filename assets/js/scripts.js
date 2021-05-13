b7b5f6e78a7901aa0ef1a29e2a2dbc91e2e88899
Robert Lester <rlester@roberts-macbook-pro.local>
2021-05-13 15:15:59 +0000

    ..

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -170,7 +170,7 @@ function appendDismissLine() {
 }
 
 const el = document.querySelector(".sticky-form")
-const observer = new IntersectionObserver(function(e) {
+const observer = new IntersectionObserver(function([e]) {
 	console.log(e);
 	e.target.classList.toggle("is-pinned", e.intersectionRatio < 1);
 }, { threshold: [1] });
