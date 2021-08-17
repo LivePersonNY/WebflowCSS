7895e1e5ece6fbe9148f496a8c28b5d9327e3a8a
Giancarlo Trentini <giancarlo.trentini@gmail.com>
2021-08-17 17:03:27 -0400

    roi calc

--- assets/js/roi-implemented.js
+++ assets/js/roi-implemented.js
@@ -126,6 +126,7 @@ function updateTableCells(cellName, valueObj, valueStyle, subKey, accuracy, accu
 			if(type == 'percent'){
 				valFloat = valFloat / 100;
 			}
+			valFloat = valFloat || 0;
 			$(this).val(locNumber(valFloat, type, undefined, minDec, maxDec));
 			updateChart();
 		})
